e4c9050aaaab5b0415fa27013d4eb26d08bb54d8
Jose Carlos Ruiz Sanchez <jrs407@inlumine.ual.es>
2024-11-02 13:34:02 +0100

    Añadida base de datos

--- backend/index.js
+++ backend/index.js
@@ -1,2 +1,23 @@
 // index.js
 console.log('Hola, Node.js!');
+
+const mysql = require('mysql2');
+
+const pool = mysql.createPool({
+  host: 'localhost',
+  user: 'root',
+  password: 'root',
+  database: 'pris1'
+});
+
+async function getClientes() {
+    try {
+      const [rows] = await pool.promise().query('SELECT * FROM Cliente');
+      console.log('Resultados de la consulta:');
+      console.log(rows);
+    } catch (error) {
+      console.error('Error al ejecutar la consulta:', error);
+    }
+  }
+  
+  getClientes();
