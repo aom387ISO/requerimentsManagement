5e98e08436a9ec495ea21a0e44bbf9704c19b36e
Antonio Ordóñez Molina <aom387@inlumine.ual.es>
2024-11-10 19:35:09 +0100

    Actualización react-router-dom

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -9,14 +9,15 @@ import EliminarCliente from './eliminarCliente';
 import InicioAdmin from './inicioAdmin';
 import AnadirTarea from './anadirTarea'
 import EditarTarea from './editarTarea'
+import VistaListaDeProyectoAnadirCliente from './vistaListaDeProyectoAnadirCliente';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <IniciarSesion/>
+    <VistaListaDeProyectoAnadirCliente/>
   </React.StrictMode>
 );
-// import VistaListaDeProyectoAnadirCliente from './vistaListaDeProyectoAnadirCliente';
+// 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
